f059c72e27b5ce4c04f8dced8fcf7a9b06351e80
hayato1118 <hayato.nakamura1118@gmail.com>
2018-01-24 08:12:39 +0000

    kadai3-9_ver2_finish

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
+  layout 'not_modal'
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-devise_for :users
+devise_for :users, :controllers => {
+ :registrations => 'users/registrations',
+ :sessions => 'users/sessions'
+}
 get 'users/top/:id' => 'users#top', as: 'user_infomation'
 resources :projects
 resources :users
@@ -7,14 +10,11 @@ resources :users, only: [:show]
 resources :books
 get"home/about"
 resources :post_images, only: [:new, :create, :index, :show]
-
 resources :books, only: [:new, :create, :index, :show] do
 resource :post_comments, only: [:create, :destroy]
 end
-
 resources :books, only: %w(create)
 resources :books, only: %w(index create)
-
 root 'post_images#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
